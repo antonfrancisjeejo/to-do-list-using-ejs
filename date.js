fcd50b56fc1c94f878b3e1063ff0bec7f03fb42a
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-04-01 00:57:46 +0530

    Bugs fixed

--- date.js
+++ date.js
@@ -1,24 +1,12 @@
 //module.exports or use exports
 exports.getDate = function() {
   //Creates a new date object with current date,month and year
-  const today = new Date();
+  let today = new Date();
   //Javascript object to convert the date to string
   const options = {
     weekday: "long",
     day: "numeric",
     month: "long"
-  };
-  //Inbuilt function to convert the date and month to en-us that is Day Month and date
-  return today.toLocaleDateString("en-us", options);
-
-};
-
-exports.getDay = function() {
-  //Creates a new date object with current date,month and year
-  const today = new Date();
-  //Javascript object to convert the date to string
-  const options = {
-    weekday: "long"
   };
   //Inbuilt function to convert the date and month to en-us that is Day Month and date
   return today.toLocaleDateString("en-us", options);
